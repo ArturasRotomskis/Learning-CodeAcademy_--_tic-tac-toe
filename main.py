ff45b5dd2501affacba628d9422d58ccab1f2ba1
Artūras Rotomskis <112565835+ArturasRotomskis@users.noreply.github.com>
2025-03-17 19:16:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -18,4 +18,4 @@ def print_list(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_list('procedūrinis, funkcinis, objektinis')
+    print_list('procedūrinis, funkcinis')
